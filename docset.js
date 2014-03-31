87a7153b3f05b594b786984bc929fa2cb93aa5bd
Jeroen Knoef <jeroen@neoneo.nl>
2014-03-31 14:22:22 +0200

    Moved copying of files

--- docset.js
+++ docset.js
@@ -1,14 +1,15 @@
+var fs = require("fs");
 var exec = require("child_process").exec;
 var sqlite3 = require("sqlite3");
-var fs = require("fs");
 
 exec("phantomjs " + __dirname + "/scrape.js", function (error, stdout, stderr) {
+
+	fs.writeFileSync(process.cwd() + "/GoogleMaps.docset/Contents/Info.plist", fs.readFileSync(__dirname + "/Info.plist"));
+	fs.writeFileSync(process.cwd() + "/GoogleMaps.docset/icon.png", fs.readFileSync(__dirname + "/Icon.png"));
 
 	var toc = JSON.parse(stdout);
 	var db = new sqlite3.Database(process.cwd() + "/GoogleMaps.docset/Contents/Resources/docSet.dsidx", function (error) {
 
-		fs.writeFileSync(process.cwd() + "/GoogleMaps.docset/Contents/Info.plist", fs.readFileSync(__dirname + "/Info.plist"));
-		fs.writeFileSync(process.cwd() + "/GoogleMaps.docset/icon.png", fs.readFileSync(__dirname + "/Icon.png"));
 
 		db.serialize(function () {
 			db.exec("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);");
